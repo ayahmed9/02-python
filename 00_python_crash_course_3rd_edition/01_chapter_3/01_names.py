e1697e69ae19fcd28e555c4aebb1ae253504ec19
Alvi Ahmed <ayahmed9@outlook.com>
2025-12-07 21:21:42 -0600

    Exploring list options
    Explored list options and renamed incorrectly named files

--- 00_python_crash_course_3rd_edition/01_chapter_3/01_names.py
+++ 00_python_crash_course_3rd_edition/01_chapter_3/01_names.py
@@ -1,1 +1,12 @@
+names = ['lilo', 'stitch', 'miles', 'pavitr']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
 
+# Can also code it in reverse
+
+print(names[-1])
+print(names[-2])
+print(names[-3])
+print(names[-4])
