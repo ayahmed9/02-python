700cd9afe994259e53e050825c990f1de5dce8da
Alvi Ahmed <ayahmed9@outlook.com>
2025-12-07 23:41:14 -0600

    Further exploring lists with complexity.

--- 00_python_crash_course_3rd_edition/01_chapter_3/02_greetings.py
+++ 00_python_crash_course_3rd_edition/01_chapter_3/02_greetings.py
@@ -1,1 +1,5 @@
-
+names = ['lilo', 'stitch', 'miles', 'pavitr']
+print(f"Welcome to Python, {names[0].title()}!")
+print(f"Welcome to Python, {names[1].title()}!")
+print(f"Welcome to Python, {names[2].title()}!")
+print(f"Welcome to Python, {names[3].title()}!")
--- 00_python_crash_course_3rd_edition/01_chapter_3/03_my_own_list.py
+++ 00_python_crash_course_3rd_edition/01_chapter_3/03_my_own_list.py
@@ -1,1 +1,8 @@
-
+cars = ['Nissan GTR Skyline', 'McLaren P1', 'Koennigsegg Gemera', 'Cadillac CTS Blackwing', 'mazda', 'Mazda Rx-9', 'Mazda Rx-8', 'Mazda Rx-7', 'rx', 'Subaru BRZ']
+print(f"My ultimate dream car is none other than the {cars[0]} from Fast & Furious.")
+print(f"A car that I dream of owning is originally a {cars[1]} but now also a {cars[2]}.")
+print(f"I hope to one day own a {cars[3]} and a {cars[5]}.")
+print(f"The {cars[5]} is a modern continuation of the famous {cars[-2].title()} line.")
+print(f"The famous {cars[-2].title()} line by {cars[4].title()} was featured in the popular game Need for Speed Carbon.")
+print(f"In my favorite game Need for Speed Carbon, I would always drive either a {cars[-4]} or a {cars[-3]}.")
+print(f"In another iteration of the game, I would drive the {cars[-1]}.")
